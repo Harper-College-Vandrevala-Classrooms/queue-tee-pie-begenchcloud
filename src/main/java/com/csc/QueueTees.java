73ab1cfc0d61558fe05602ea8a2223a14a474182
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-11-28 00:54:50 -0600

    complete

--- src/main/java/com/csc/QueueTees.java
+++ src/main/java/com/csc/QueueTees.java
@@ -2,7 +2,46 @@ package com.csc;
 
 public class QueueTees {
 
-  public static void main(String[] args) {
-    System.out.println("So Cute!");
+  private Cutie[] queue;
+  private int front;
+  private int rear;
+  private int size;
+  private final int capacity;
+
+  public QueueTees(int capacity) {
+    this.capacity = capacity;
+    this.queue = new Cutie[capacity];
+    this.front = 0;
+    this.rear = -1;
+    this.size = 0;
+  }
+
+  public QueueTees() {
+    this(5);
+  }
+
+  public void enqueue(Cutie cutie) {
+    if (size == capacity) {
+      System.out.println("The queue is full. Cannot add more cuties!");
+      return;
+    }
+    rear = (rear + 1) % capacity;
+    queue[rear] = cutie;
+    size++;
+  }
+
+  public Cutie dequeue() {
+    if (size == 0) {
+      System.out.println("The queue is empty. No cuties to remove!");
+      return null;
+    }
+    Cutie removed = queue[front];
+    front = (front + 1) % capacity;
+    size--;
+    return removed;
+  }
+
+  public int size() {
+    return size;
   }
 }
--- src/test/java/com/csc/TestQueueTees.java
+++ src/test/java/com/csc/TestQueueTees.java
@@ -1,7 +1,7 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,13 +9,70 @@ public class TestQueueTees {
 
   QueueTees queue;
 
+  class Puppy implements Cutie {
+    public String description() {
+      return "A little puppy with big, sad eyes";
+    }
+
+    public Integer cutenessRating() {
+      return 11;
+    }
+  }
+
+  class Kitty implements Cutie {
+    public String description() {
+      return "A fluffy kitten with tiny whiskers";
+    }
+
+    public Integer cutenessRating() {
+      return 10;
+    }
+  }
+
   @BeforeEach
   void setUp() {
-    queue = new QueueTees();
+    queue = new QueueTees(3);
   }
 
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void testEnqueueAndSize() {
+    Cutie puppy = new Puppy();
+    Cutie kitty = new Kitty();
+
+    queue.enqueue(puppy);
+    queue.enqueue(kitty);
+
+    assertEquals(2, queue.size());
+  }
+
+  @Test
+  void testDequeue() {
+    Cutie puppy = new Puppy();
+    Cutie kitty = new Kitty();
+
+    queue.enqueue(puppy);
+    queue.enqueue(kitty);
+
+    assertEquals(puppy, queue.dequeue());
+    assertEquals(kitty, queue.dequeue());
+  }
+
+  @Test
+  void testQueueFull() {
+    Cutie puppy = new Puppy();
+    Cutie kitty = new Kitty();
+
+    queue.enqueue(puppy);
+    queue.enqueue(kitty);
+    queue.enqueue(puppy);
+
+    queue.enqueue(kitty);
+
+    assertEquals(3, queue.size());
+  }
+
+  @Test
+  void testDequeueEmpty() {
+    assertNull(queue.dequeue());
   }
 }
